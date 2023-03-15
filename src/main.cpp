779d6b0c99aa4712dfdfc46085ce37be4a79627c
IsaiasGatica <isaiasgatica1@gmail.com>
2023-03-15 20:59:59 -0300

    No funcional (No encuentra otro nodo) Probando nodo raíz.

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,102 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp-mesh-esp32-esp8266-painlessmesh/
 
-  This is a simple example that uses the painlessMesh library: https://github.com/gmag11/painlessMesh/blob/master/examples/basic/basic.ino
-*/
-
-#include "painlessMesh.h"
+#include <painlessMesh.h>
+#include <PubSubClient.h>
+#include <WiFiClient.h>
 
 #define MESH_PREFIX "whateverYouLike"
 #define MESH_PASSWORD "somethingSneaky"
 #define MESH_PORT 5555
 
-Scheduler userScheduler; // to control your personal task
+#define STATION_SSID "GWIFI"
+#define STATION_PASSWORD "independenciaGatica"
+
+#define HOSTNAME "MQTT_Bridge"
+
+const char *mqtt_broker = "broker.emqx.io";
+const char *topic = "esp32/message";
+const char *mqtt_username = "";
+const char *mqtt_password = "";
+const int mqtt_port = 1883;
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+// Prototypes
+void receivedCallback(const uint32_t &from, const String &msg);
+void MQTTcallback(char *topic, byte *payload, unsigned int length);
+
 painlessMesh mesh;
-
-// User stub
-void sendMessage(); // Prototype so PlatformIO doesn't complain
-
-Task taskSendMessage(TASK_SECOND * 1, TASK_FOREVER, &sendMessage);
-
-void sendMessage()
-{
-  String msg = "Hi from node1 ";
-  msg += mesh.getNodeId();
-  mesh.sendBroadcast(msg);
-  taskSendMessage.setInterval(random(TASK_SECOND * 1, TASK_SECOND * 5));
-}
-
-// Needed for painless library
-void receivedCallback(uint32_t from, String &msg)
-{
-  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
-}
-
-void newConnectionCallback(uint32_t nodeId)
-{
-  Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
-}
-
-void changedConnectionCallback()
-{
-  Serial.printf("Changed connections\n");
-  String Nodos = mesh.subConnectionJson();
-  Serial.println(Nodos);
-}
-
-void nodeTimeAdjustedCallback(int32_t offset)
-{
-  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
-}
 
 void setup()
 {
+
   Serial.begin(115200);
+  WiFi.begin(STATION_SSID, STATION_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network!");
 
-  // mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-  mesh.setDebugMsgTypes(ERROR | STARTUP); // set before init() so that you can see startup messages
+  mqttClient.setServer(mqtt_broker, mqtt_port);
+  mqttClient.setCallback(MQTTcallback);
+  while (!mqttClient.connected())
+  {
+    String client_id = String(WiFi.macAddress());
 
-  mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
+    if (mqttClient.connect(client_id.c_str(), mqtt_username, mqtt_password))
+    {
+      Serial.println("Connected to Public MQTT Broker");
+    }
+    else
+    {
+      Serial.print("Failed to connect with MQTT Broker");
+      Serial.print(mqttClient.state());
+      delay(2000);
+    }
+  }
+
+  mqttClient.publish(topic, "Hello! I am ESP32");
+  mqttClient.subscribe(topic);
+
+  mesh.setDebugMsgTypes(ERROR | STARTUP | CONNECTION); // set before init() so that you can see startup messages
+
+  // Channel set to 6. Make sure to use the same channel for your mesh and for you other
+  // network (STATION_SSID)
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT, WIFI_AP_STA, 6);
   mesh.onReceive(&receivedCallback);
-  mesh.onNewConnection(&newConnectionCallback);
-  mesh.onChangedConnections(&changedConnectionCallback);
-  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
 
-  userScheduler.addTask(taskSendMessage);
-  taskSendMessage.enable();
+  mesh.stationManual(STATION_SSID, STATION_PASSWORD);
+  mesh.setHostname(HOSTNAME);
+
+  // Bridge node, should (in most cases) be a root node. See [the wiki](https://gitlab.com/painlessMesh/painlessMesh/wikis/Possible-challenges-in-mesh-formation) for some background
+  mesh.setRoot(true);
+  // This node and all other nodes should ideally know the mesh contains a root, so call this on all nodes
+  mesh.setContainsRoot(true);
 }
 
 void loop()
 {
-  // it will run the user scheduler as well
+
+  mqttClient.loop();
   mesh.update();
+}
+
+void receivedCallback(const uint32_t &from, const String &msg)
+{
+  Serial.printf("bridge: Received from %u msg=%s\n", from, msg.c_str());
+}
+
+void MQTTcallback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  Serial.println("************");
 }
