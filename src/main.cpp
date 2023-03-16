e940e0d659af4f1b58ceefa597ff76bfdc1231d2
IsaiasGatica <isaiasgatica1@gmail.com>
2023-03-16 03:02:29 -0300

    Se comunica que existe un nodo raiz

--- src/main.cpp
+++ src/main.cpp
@@ -56,7 +56,7 @@ void setup()
 
   // mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
   mesh.setDebugMsgTypes(ERROR | STARTUP); // set before init() so that you can see startup messages
-  mesh.setContainsRoot();
+  mesh.setContainsRoot(true);
   mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
   mesh.onReceive(&receivedCallback);
   mesh.onNewConnection(&newConnectionCallback);
