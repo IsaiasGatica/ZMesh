c0b3bfa7fa83ccc3393c107a3f0ec743f2b938d4
IsaiasGatica <isaiasgatica1@gmail.com>
2023-03-21 14:06:50 -0300

    Nodo Colector/Nodo Sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,41 @@
 /*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp-mesh-esp32-esp8266-painlessmesh/
-
-  This is a simple example that uses the painlessMesh library: https://github.com/gmag11/painlessMesh/blob/master/examples/basic/basic.ino
+  Nodo Colector / Nodo Sensor
 */
 
 #include "painlessMesh.h"
+#include <ArduinoJson.h>
 
 #define MESH_PREFIX "whateverYouLike"
 #define MESH_PASSWORD "somethingSneaky"
 #define MESH_PORT 5555
 
+// #define NodoSensor
+
+#define RXD2 16
+#define TXD2 17
+
 Scheduler userScheduler; // to control your personal task
 painlessMesh mesh;
 
-// User stub
 void sendMessage(); // Prototype so PlatformIO doesn't complain
 
+#ifdef NodoSensor
 Task taskSendMessage(TASK_SECOND * 1, TASK_FOREVER, &sendMessage);
+#endif
 
-void sendMessage()
-{
-  String msg = "Hi from node4 ";
-  msg += mesh.getNodeId();
-  mesh.sendBroadcast(msg);
-  taskSendMessage.setInterval(random(TASK_SECOND * 1, TASK_SECOND * 5));
-}
-
-// Needed for painless library
 void receivedCallback(uint32_t from, String &msg)
 {
-  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+  Serial.println(msg);
+
+#ifdef NodoSensor
+  StaticJsonDocument<200> doc;
+  doc["Nodo"] = "4";
+  doc["Valor"] = 55;
+  serializeJson(doc, Serial2);
+#else
+  Serial2.println(msg);
+
+#endif
 }
 
 void newConnectionCallback(uint32_t nodeId)
@@ -53,6 +58,7 @@ void nodeTimeAdjustedCallback(int32_t offset)
 void setup()
 {
   Serial.begin(115200);
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
 
   // mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
   mesh.setDebugMsgTypes(ERROR | STARTUP); // set before init() so that you can see startup messages
@@ -63,8 +69,11 @@ void setup()
   mesh.onChangedConnections(&changedConnectionCallback);
   mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
 
+#ifdef NodoSensor
+
   userScheduler.addTask(taskSendMessage);
   taskSendMessage.enable();
+#endif
 }
 
 void loop()
@@ -72,3 +81,19 @@ void loop()
   // it will run the user scheduler as well
   mesh.update();
 }
+
+#ifdef NodoSensor
+
+void sendMessage()
+{
+  String msg;
+  StaticJsonDocument<200> doc;
+  doc["Nodo"] = "4";
+  doc["Valor"] = 55;
+  serializeJson(doc, Serial2);
+  serializeJson(doc, msg);
+  mesh.sendBroadcast(msg);
+  taskSendMessage.setInterval(random(TASK_SECOND * 1, TASK_SECOND * 5));
+}
+
+#endif
